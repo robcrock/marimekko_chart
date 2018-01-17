46f8593592fd8093c1bd9f6445afafd338b29558
robcrock <rob@robcrock.com>
2018-01-17 14:07:11 +1100

    Got sorting to work.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const margin = { top: 30, right: 10, bottom: 30, left: 300 },
   width = 700 - margin.left - margin.right,
-  height = 1600 - margin.top - margin.bottom;
+  height = 3000 - margin.top - margin.bottom;
 
-const svg = d3.select('body').append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
+const svg = d3.select('body')
+  .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-// x = function (d) { return d.offset; },
 const xScale = d3.scaleLinear().range([0, width]),
   xValue = function (d) { return xScale(x(d)); },
   xAxis = d3.axisBottom(xScale);
@@ -20,15 +20,6 @@ const state = function (d) { return d.key; },
   stateScaleG = d3.scaleBand().range([0, height]),
   statePositionG = function (d) { return stateScaleG(state(d)); },
   stateAxisG = d3.axisLeft(stateScaleG);
-
-// var area = d3.area()
-//   .x(xValue)
-//   .y1(yValue);
-
-// mbostock example
-// var area = d3.area()
-//   .x(function (d) { return x(d.date); })
-//   .y1(function (d) { return y(d.close); });
 
 d3.csv('household_income.csv', function(error, data) {
   if (error) throw error;
@@ -43,27 +34,51 @@ d3.csv('household_income.csv', function(error, data) {
     d.bar_width = +d.bar_width;
     d.number_of_households = +d.number_of_households;
     d.percent_of_total = +d.percent_of_total;
+    switch(d.income_level) {
+      case 'Less than $10,000': d.income_index = 0
+        break;
+      case '$10,000 to $14,999': d.income_index = 1
+        break;
+      case '$15,000 to $19,999': d.income_index = 2
+        break;
+      case '$20,000 to $24,999': d.income_index = 3
+        break;
+      case '$25,000 to $29,999': d.income_index = 4
+        break;
+      case '$30,000 to $34,999': d.income_index = 5
+        break;
+      case '$35,000 to $39,999': d.income_index = 6
+        break;
+      case '$40,000 to $44,999': d.income_index = 7
+        break;
+      case '$45,000 to $49,999': d.income_index = 8
+        break;
+      case '$50,000 to $59,999': d.income_index = 9
+        break;
+      case '$60,000 to $74,999': d.income_index = 10
+        break;
+      case '$75,000 to $99,999': d.income_index = 11
+        break;
+      case '$100,000 to $124,999': d.income_index = 12
+        break;
+      case '$125,000 to $149,999': d.income_index = 13
+        break;
+      case '$150,000 to $199,999': d.income_index = 14
+        break;
+      case '$200,000 or more': d.income_index = 15
+        break;
+    }
   })
 
   let dataFlat = d3.nest()
     .key(function (d) { return d.state; })
     .entries(households_2016);
 
-  // *** Don't forget about this cool trick Sort states by peak income level
-  // function peakIncomeLevel(d) {
-  //   let i = d3.scan(d.values, function (a, b) { return y(b) - y(a); });
-  //   return d.values[i];
-  // };
-
-  // dataFlat.sort(function (a, b) { return peakIncomeLevel(a) - peakIncomeLevel(b); });
-
   xScale
     .domain([0, 44]);
 
   stateScaleG
     .domain(dataFlat.map(function (d) { return d.key; }));
-
-  // console.log(percentScale.domain())
 
   const marimekkoChartHeight = height / stateScaleG.domain().length;
 
@@ -72,14 +87,20 @@ d3.csv('household_income.csv', function(error, data) {
     .domain([0, d3.max(households_2016, d => d.percent_of_total)])
     .range([marimekkoChartHeight, 0]);
 
-  // area.y0(yScale(0));
-
   svg.append('g').attr('class', 'axis axis--x')
     .attr('transform', 'translate(0,' + height + ')')
     .call(xAxis);
 
   svg.append('g').attr('class', 'axis axis--state')
     .call(stateAxisG);
+
+  dataFlat.forEach(function (d) {
+
+    d.values.sort(function (a, b) {
+      return a.percent_of_total - b.percent_of_total;
+    });
+
+  })
 
   const gState = svg
     .append('g')
@@ -101,13 +122,6 @@ d3.csv('household_income.csv', function(error, data) {
     })
   });
 
-  // dataFlat.reduce(function (v, p) {
-  //   console.log(p.values);
-  //   return (p = v) + p.bar_width;
-  // }, 0);
-
-  // console.log(dataFlat);
-
   gState.selectAll('.bar')
     .data(d => d.values)
     .enter().append("rect")
@@ -119,60 +133,65 @@ d3.csv('household_income.csv', function(error, data) {
     .attr("y", d => yScale(d.percent_of_total))
     .style("fill", '#ccc');
 
-  // console.log(dataFlat.values);
-
-  // gState.append('path').attr('class', 'area')
-  //   .datum(function (d) { return d.values; })
-  //   .attr('d', area);
-
-  // const gIncomeLevels = .selectAll('rect')
-  //   .data(d => d.values).enter()
-  //   .append('rect')
-  //     .attr('class', 'bar')
-
-    // .attr('x', x)
-    // .attr('y', y)
-    // .attr("height", d => yScale(d.percent_of_total))
-    // .attr("width", d => xScale(d.barWidth) )
-    // .style("fill", '#ccc');
-
-  // console.log(dataFlat[0].values);
-
   //////////////////// HANDY FUNCTIONS ////////////////////
-
-  // Compute bar offset
-  function computeStateOffset(array) { //, state) {
-    // console.log(array, state);
-
-    // totalOffset will equal 42
-    // let filteredArr = [];
-
-    // filteredArr = array.filter(function(d) {
-    //   return d.state === state;
-    // });
-
-    array.reduce(function (v, p) {
-      return (p.offset = v) + p.bar_width;
-    }, 0);
-
-    // console.log(extendedValues);
-
-    return extendedValues;
-
-  }
 
   // Sorts stat by income_level
   function sortStatesBySelectedIncome(data, selectedIncome) {
     let statesAtLevel = [];
 
-    statesAtLevel = data.filter(function(d) {
-      return d.income_level === selectedIncome;
-    })
+    // statesAtLevel = data.filter(function(d) {
+    //   return d.income_level === selectedIncome;
+    // })
 
-    statesAtLevel.sort(function (a, b) { return b.percent_of_total - a.percent_of_total; });
+    statesAtLevel.push(data.sort(function (a, b) { return b.percent_of_total - a.percent_of_total; }));
 
     return statesAtLevel;
   }
+
+  // *** Don't forget about this cool trick Sort states by peak income level
+  // EXPERIMENT WITH SORTING FUNCTIONS
+  
+  // log whole data set
+  // console.log(dataFlat);
+
+  // log only states
+  // dataFlat.forEach(function(d) {
+  //   console.log(d.key);
+  // })
+
+  // log sorted states
+  dataFlat.forEach(function(d) {
+
+    // console.log(d.values);
+
+    d.values.sort(function (a, b) {
+      return a.percent_of_income - b.percent_of_income;
+    });
+
+  })
+
+  console.log(dataFlat);
+
+  // dataFlat.forEach(function (d) {
+  //   sortStatesBySelectedIncome(d.values, '$200,000 or more')
+  // });
+
+  // dataFlat.sort(function (a, b) { return a.time - b.time; });
+
+  // dataFlat.forEach(function (d) {
+  //   d.values.sort(function (a, b) {
+  //     return a.income_index - b.income_index;
+  //   })
+  // });
+
+  // d3.scan(dataFlat[0].values, function (a, b) {
+  //   return b.percent_of_total - a.percent_of_total;
+  // })
+
+  // function peakIncomeLevel(d) {
+  //   let i = d3.scan(d.values, function (a, b) { return y(b.percent_of_total) - y(a.percent_of_total); });
+  //   return d.values[i];
+  // };
 
   // TEST RUNS OF HANDY FUNCTIONS
   // console.log(sortStatesBySelectedIncome(households_2016, '$10,000 to $14,999'));
